a53a7fb245e20015fa242997a49541a48ce63017
CodingArchitect <sendhil.r@gmail.com>
2013-03-21 21:02:02 +0530

    Refactored to remove duplication. Added a conversion factors lookup and used it represent the logic in a generic fashion.

--- src/CodingArchitect.Katas/RomanNumeralConverter.cs
+++ src/CodingArchitect.Katas/RomanNumeralConverter.cs
@@ -7,19 +7,23 @@ namespace CodingArchitect.Katas
 {
     public static class RomanNumeralConverter
     {
+        private static readonly IDictionary<int, string> ConversionFactors = new Dictionary<int, string>
+        {
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
+        };
+
+        private static readonly IEnumerable<int> Arabics = ConversionFactors.Keys.OrderByDescending(a => a);
+
         public static string ToRoman(this int arabicNumeral)
         {
             if(arabicNumeral == 0)
                 return "";
-            if (arabicNumeral >= 10)
-                return "X" + ToRoman(arabicNumeral - 10);
-            if (arabicNumeral >= 9)
-                return "IX" + ToRoman(arabicNumeral - 9);
-            if (arabicNumeral >= 5)
-                return "V" + ToRoman(arabicNumeral - 5);
-            if (arabicNumeral >= 4)
-                return "IV" + ToRoman(arabicNumeral - 4);            
-            return "I" + ToRoman(arabicNumeral - 1);
+            var arabic = Arabics.First(a => arabicNumeral >= a);
+            return ConversionFactors[arabic] + ToRoman(arabicNumeral - arabic);
         }
     }
 }
