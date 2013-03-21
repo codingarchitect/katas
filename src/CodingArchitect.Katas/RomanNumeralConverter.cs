1841579dd1ee3e4996633a48b93b90e515889fda
CodingArchitect <sendhil.r@gmail.com>
2013-03-21 20:20:53 +0530

    Initial implementation Added a failing test for 0 => "" case

---
+++ src/CodingArchitect.Katas/RomanNumeralConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodingArchitect.Katas
+{
+    public static class RomanNumeralConverter
+    {
+        public static string ToRoman(this int arabicNumeral)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/CodingArchitect.Katas/RomanNumeralConverterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodingArchitect.Katas
+{
+    [TestFixture]
+    public class RomanNumeralConverterTests
+    {
+        [Test]
+        public void Test0ReturnsEmpty()
+        {
+            var roman = 0.ToRoman();
+            Assert.AreEqual("", roman);
+        }
+    }
+}
